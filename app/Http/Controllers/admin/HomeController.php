e8285a365b558a4a0ca228d6779d69fae705cce6
nurkhalisa <lisatohir@gmail.com>
2024-03-04 20:59:12 +0800

    Update admin dashboard

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -2,14 +2,59 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Brand;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('admin.dashboard');
+
+        $totalCategories = Category::count();
+        $totalBrands = Brand::count();
+        $totalProducts = Product::count();
+        $totalCustomers = User::where('role',1)->count();
+        $totalOrders = Order::where('status','!=','cancelled')->count();
+        $totalSales = Order::where('status','!=','cancelled')->sum('grand_total');
+
+        //This month sales
+        $startOfMonth = Carbon::now()->startOfMonth()->format('Y-m-d');
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $thisMonthName = Carbon::now()->startOfMonth()->format('M');
+
+        $totalSalesThisMonth = Order::where('status','!=','cancelled')
+                            ->whereDate('created_at','>=',$startOfMonth)
+                            ->whereDate('created_at','<=',$currentDate)
+                            ->sum('grand_total');
+
+        //Last month sales
+        $lastMonthStartDate = Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
+        $lastMonthEndDate = Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d');
+        $lastMonthName = Carbon::now()->subMonth()->startOfMonth()->format('M');
+
+        $totalSalesLastMonth = Order::where('status','!=','cancelled')
+                            ->whereDate('created_at','>=',$lastMonthStartDate)
+                            ->whereDate('created_at','<=',$lastMonthEndDate)
+                            ->sum('grand_total');
+
+        return view('admin.dashboard',[
+            'totalCategories' => $totalCategories,
+            'totalBrands' => $totalBrands,
+            'totalProducts' => $totalProducts,
+            'totalCustomers' => $totalCustomers,
+            'totalOrders' => $totalOrders,
+            'totalSales' => $totalSales,
+            'totalSalesThisMonth' => $totalSalesThisMonth,
+            'thisMonthName' => $thisMonthName,
+            'totalSalesLastMonth' => $totalSalesLastMonth,
+            'lastMonthName' => $lastMonthName
+        ]);
         //$admin = Auth::guard('admin')->user();
         //echo 'Welcome '.$admin->name.' <a href="'.route('admin.logout').'">Logout</a>';
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -23,20 +23,46 @@
             <div class="col-lg-4 col-6">							
                 <div class="small-box card">
                     <div class="inner">
-                        <h3>150</h3>
-                        <p>Total Orders</p>
+                        <h3>{{ $totalCategories }}</h3>
+                        <p>Total Categories</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('categories.index') }}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-6">							
+                <div class="small-box card">
+                    <div class="inner">
+                        <h3>{{ $totalBrands }}</h3>
+                        <p>Total Brands</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{ route('brands.index') }}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             
             <div class="col-lg-4 col-6">							
                 <div class="small-box card">
                     <div class="inner">
-                        <h3>50</h3>
+                        <h3>{{ $totalProducts }}</h3>
+                        <p>Total Products</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="{{ route('products.index') }}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-6">							
+                <div class="small-box card">
+                    <div class="inner">
+                        <h3>{{ $totalCustomers }}</h3>
                         <p>Total Customers</p>
                     </div>
                     <div class="icon">
@@ -45,12 +71,51 @@
                     <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+
+            <div class="col-lg-4 col-6">							
+                <div class="small-box card">
+                    <div class="inner">
+                        <h3>{{ $totalOrders }}</h3>
+                        <p>Total Orders</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="{{ route('orders.index') }}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
             
             <div class="col-lg-4 col-6">							
                 <div class="small-box card">
                     <div class="inner">
-                        <h3>$1000</h3>
-                        <p>Total Sale</p>
+                        <h3>RM {{ number_format($totalSales,2) }}</h3>
+                        <p>Total Sales</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-6">							
+                <div class="small-box card">
+                    <div class="inner">
+                        <h3>RM {{ number_format($totalSalesThisMonth,2) }}</h3>
+                        <p>Total Sales This Month ({{ $thisMonthName }})</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-6">							
+                <div class="small-box card">
+                    <div class="inner">
+                        <h3>RM {{ number_format($totalSalesLastMonth,2) }}</h3>
+                        <p>Total Sales Last Month ({{ $lastMonthName }})</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
